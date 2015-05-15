791775281fea138470a3e0c320f1033f6c62ad03
Sergej Charskij <sergej.charskij@gmail.com>
2015-05-16 00:36:46 +0100

    add zsh to dotfiles

--- dotfiles/dotfiles.sh
+++ dotfiles/dotfiles.sh
@@ -5,6 +5,7 @@ filename=$HOME/dotfiles
 if [ ! -d $filename ]; then
     git clone https://github.com/sergejc/dotfiles.git $HOME/dotfiles
     sed -i '1s/^/source ~\/dotfiles\/bash\/bashrc\n/' $HOME/.zshrc
+    echo 'source dotfiles/bash/bashrc' >> $HOME/.zshrc
 fi
 
 # Vim config
