888fde3f51f9d3e1ca0e7ddc589673b6bd217374
Sergej Charskij <sergej.charskij@gmail.com>
2015-12-30 12:28:47 +0000

    add latest nodejs 5.3

--- nodejs/nodejs.sh
+++ nodejs/nodejs.sh
@@ -3,22 +3,32 @@
 # NodeJs
 filename='/etc/apt/sources.list.d/nodesource.list'
 if [ ! -f $filename ]; then
-    curl -sL https://deb.nodesource.com/setup | sudo bash -
+    curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
     sudo apt-get install nodejs -y
 fi
 
 # NVM
 if [ ! -f $HOME/.nvm/nvm.sh ]; then
-    curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash 
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 
     source ~/.nvm/nvm.sh
-    nvm install 0.12.0 
+    nvm install 5.3.0
 
     # Packages
-    npm install -g grunt-cli bower jade coffee-script express express-generator jasmine-node mocha yo browserify beefy gulp react-tools grunt-init http-server jshint stylus node-sass less autoprefixer clean-css mean-cli requirejs supervisor node-inspector babel
+    npm install -g bower jade express supervisor babel express-generator react-tools http-server mean-cli node-inspector
+
+    # build tools
+    npm install -g yo browserify gulp requirejs jspm webpack grunt-init grunt-cli
+
+    # css processors
+    npm install -g stylus less node-sass clean-css
+
+    # test tools
+    npm install -g mocha eslint jasmine-node
+
 
     echo "source $HOME/.nvm/nvm.sh" >> $HOME/.zshrc
-    echo "nvm use 0.12 >> /dev/null" >> $HOME/.zshrc
-    
+    echo "nvm use 5.3.0 >> /dev/null" >> $HOME/.zshrc
+
 fi
 
