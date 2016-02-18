87c74aaa8808484759fb20226e2c3ecbf349289a
Sergej Charskij <sergej.charskij@gmail.com>
2016-02-18 21:08:25 +0000

    add tmux

---
+++ tmux/tmux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ ! -d $HOME/tmux-powerline ]; then
+    #git clone https://github.com/sergejc/tmux-powerline.git $HOME/.tmux-powerline
+    cat $(dirname $0)/tmux.conf > $HOME/.tmux.conf
+fi
