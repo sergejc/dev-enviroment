2a8025d27b5a04bae6bf453612ea02e624061f30
Sergej Charskij <sergej.charskij@gmail.com>
2016-04-23 20:17:13 +0100

    add tmux plugin installer

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -2,5 +2,6 @@
 
 if [ ! -d $HOME/tmux-powerline ]; then
     git clone https://github.com/sergejc/tmux-powerline.git $HOME/.tmux-powerline
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     cat $(dirname $0)/tmux.conf > $HOME/.tmux.conf
 fi
