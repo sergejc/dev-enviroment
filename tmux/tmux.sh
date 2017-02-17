d3c7ba8aa8dafe8a350b4d19688f6305911ff1cc
Sergej Charskij <sergej.charskij@gmail.com>
2017-02-17 06:59:31 +0000

    fixed tmux installation

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d $HOME/tmux-powerline ]; then
+if [ ! -d $HOME/.tmux-powerline ]; then
     git clone https://github.com/sergejc/tmux-powerline.git $HOME/.tmux-powerline
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     cat $(dirname $0)/tmux.conf > $HOME/.tmux.conf
