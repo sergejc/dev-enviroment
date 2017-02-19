6d4104492a726c8449d99c18d5c08c17886b895f
Sergej Charskij <sergej.charskij@gmail.com>
2017-02-19 09:55:11 +0000

    fixed tmux config

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -3,7 +3,7 @@
 if [ ! -d $HOME/.tmux-powerline ]; then
     git clone https://github.com/sergejc/tmux-powerline.git $HOME/.tmux-powerline
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    $HOME/dev-enviroment/tmux/tmux.conf > $HOME/.tmux.conf
+    cp $HOME/dev-enviroment/tmux/tmux.conf $HOME/.tmux.conf
 fi
 
 sudo apt autoremove -y
