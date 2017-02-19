7fb9bf6cea7f0a7ef12d71ceb49c7905e26aef87
Sergej Charskij <sergej.charskij@gmail.com>
2017-02-19 11:47:50 +0000

    added tmux installation

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ ! -d $HOME/.tmux-powerline ]; then
+    sudo apt install tmux -y
     git clone https://github.com/sergejc/tmux-powerline.git $HOME/.tmux-powerline
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     cp $HOME/dev-enviroment/tmux/tmux.conf $HOME/.tmux.conf
