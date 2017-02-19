3158f5052ccbcab83bfaa8902c3411eda74e1c26
Sergej Charskij <sergej.charskij@gmail.com>
2017-02-19 11:15:05 +0000

    changed dotfiles entry point

--- dotfiles/dotfiles.sh
+++ dotfiles/dotfiles.sh
@@ -5,6 +5,6 @@ filename=$HOME/dotfiles
 if [ ! -d $filename ]; then
     git clone https://github.com/sergejc/dotfiles.git $HOME/dotfiles
     sed -i '1s/^/source ~\/dotfiles\/bash\/bashrc\n/' $HOME/.zshrc
-    echo "source $HOME/dotfiles/bash/bashrc.sh" >> $HOME/.zshrc
+    echo "source $HOME/dotfiles/bash/main.sh" >> $HOME/.zshrc
 fi
 
