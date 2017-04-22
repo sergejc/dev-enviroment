0f5decffce809eff757c00b837ff6d80647a89dc
Sergej Charskij <sergej.charskij@gmail.com>
2017-04-22 18:41:36 +0100

    added rbenv

--- ruby/ruby.sh
+++ ruby/ruby.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+
 git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshrc
 echo 'eval "$(rbenv init -)"' >> $HOME/.zshrc
-exec $SHELL
 
 git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> $HOME/.zshrc
-exec $SHELL
 
-gem install bundler
-get install pry
+source $HOME/.zshrc
