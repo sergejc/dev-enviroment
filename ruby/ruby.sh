8238997d949a72fa86a862fea6a3ad8cba5b9bae
Sergej Charskij <sergej.charskij@gmail.com>
2017-04-22 17:30:16 +0100

    added rbenv

--- ruby/ruby.sh
+++ ruby/ruby.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshrc
+echo 'eval "$(rbenv init -)"' >> $HOME/.zshrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> $HOME/.zshrc
+exec $SHELL
+
+gem install bundler
 get install pry
