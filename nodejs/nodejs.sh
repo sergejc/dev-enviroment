0ae16040229c0b0265fb843911744f86e8b158ac
Sergej Charskij <sergej.charskij@gmail.com>
2017-06-01 13:27:15 +0100

    updated node version

--- nodejs/nodejs.sh
+++ nodejs/nodejs.sh
@@ -8,7 +8,7 @@ done
 # NodeJs
 filename='/etc/apt/sources.list.d/nodesource.list'
 if [ ! -f $filename ]; then
-    curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     sudo apt install nodejs -y
     sudo apt install npm -y
     sudo apt autoremove -y
