14ee611b0f13e56fe0ead1b496448e62d9c5c895
Sergej Charskij <sergej.charskij@gmail.com>
2017-08-05 17:04:42 +0100

    added ZSH_CUSTOM variable

--- shell/shell.sh
+++ shell/shell.sh
@@ -25,6 +25,7 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
     # OH MY ZSHELL PLUGINS
+    ZSH_CUSTOM='.oh-my-zsh/custom'
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
